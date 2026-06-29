e2054d8b18268d59dfda643ecf78c34ffb17e3f2
stefan jankovic <stefapro99@gmail.com>
2026-06-29 17:07:37 +0200

    created Song class and printed first song to console

--- LyricsNotebook/Program.cs
+++ LyricsNotebook/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LyricsNotebook;
+
+Song mySong = new Song("My First Song", "These are my first lyrics");
+
+Console.WriteLine("Title: " + mySong.Title);
+Console.WriteLine("Lyrics: " + mySong.Lyrics);
+Console.WriteLine("date: " + mySong.DateCreated);
+
+
+
---
+++ LyricsNotebook/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace LyricsNotebook
+{
+    internal class Song
+    {
+        public string Title {  get; set; }
+        public string Lyrics { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public Song(string title, string lyrics) 
+        { 
+            Title = title;
+            Lyrics = lyrics;
+            DateCreated = DateTime.Now;
+        }
+
+    }
+}
